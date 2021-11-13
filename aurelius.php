541745a6d7d76ab899132205d74bd4902d808806
Aubrey Portwood <code@aubreypwd.com>
2021-11-12 20:15:17 -0700

    Initial setup.

---
+++ aurelius.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name:     Aurelius
+ * Description:     A Gutenberg block that displays quotes by Marcus Aurelius.
+ * Version:         1.0.0
+ * Author:          Aubrey Portwood <code@aubreypwd.com>
+ * License:         MIT
+ * Text Domain:     aubreypwd-aurelius
+ *
+ * @since           Nov 12, 2021
+ * @package         aubreypwd\Aurelius
+ */
+
+namespace aubreypwd\Aurelius;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Register the block with WordPress.
+ *
+ * @author aubreypwd
+ * @since 0.1.0
+ */
+function register_block() {
+
+	// Define our assets.
+	$editor_script   = 'build/index.js';
+	$editor_style    = 'build/index.css';
+	$frontend_style  = 'build/style-index.css';
+	$frontend_script = 'build/frontend.js';
+
+	// Verify we have an editor script.
+	if ( ! file_exists( plugin_dir_path( __FILE__ ) . $editor_script ) ) {
+		wp_die( esc_html__( 'Whoops! You need to run `npm run build` for the aubreypwd Aurelius first.', 'aurelius' ) );
+	}
+
+	// Autoload dependencies and version.
+	$asset_file = require plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+
+	// Register editor script.
+	wp_register_script(
+		'aubreypwd-aurelius-editor-script',
+		plugins_url( $editor_script, __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version'],
+		true
+	);
+
+	// Register editor style.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $editor_style ) ) {
+		wp_register_style(
+			'aubreypwd-aurelius-editor-style',
+			plugins_url( $editor_style, __FILE__ ),
+			[ 'wp-edit-blocks' ],
+			filemtime( plugin_dir_path( __FILE__ ) . $editor_style )
+		);
+	}
+
+	// Register frontend style.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_style ) ) {
+		wp_register_style(
+			'aubreypwd-aurelius-style',
+			plugins_url( $frontend_style, __FILE__ ),
+			[],
+			filemtime( plugin_dir_path( __FILE__ ) . $frontend_style )
+		);
+	}
+
+	// Register block with WordPress.
+	register_block_type( 'aubreypwd/aurelius', array(
+		'editor_script' => 'aubreypwd-aurelius-editor-script',
+		'editor_style'  => 'aubreypwd-aurelius-editor-style',
+		'style'         => 'aubreypwd-aurelius-style',
+	) );
+
+	// Register frontend script.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_script ) ) {
+		wp_enqueue_script(
+			'aubreypwd-aurelius-frontend-script',
+			plugins_url( $frontend_script, __FILE__ ),
+			$asset_file['dependencies'],
+			$asset_file['version'],
+			true
+		);
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\register_block' );
---
+++ src/block/aurelius/index.js
@@ -0,0 +1,31 @@
+import edit from './edit';
+import save from './save';
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+import './style.scss';
+
+/**
+ * Register block type definition.
+ *
+ * @author WebDevStudios
+ * @since 0.0.1
+ * @link https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
+ */
+registerBlockType( 'aubreypwd/aurelius', {
+	title: __( 'aubreypwd Aurelius', 'aurelius' ),
+	icon: 'edit',
+	category: 'common',
+	keywords: [
+		__( 'aubreypwd', 'aurelius' ),
+		__( 'Aurelius', 'aurelius' ),
+	],
+	attributes: {
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: 'p',
+		},
+	},
+	edit,
+	save,
+} );
