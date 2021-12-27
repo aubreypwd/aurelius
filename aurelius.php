ea3959265f32048eb713afc208ab47836cb38840
Aubrey Portwood <code@aubreypwd.com>
2021-12-27 14:46:47 -0700

    Don't call this this many times.

--- aurelius.php
+++ aurelius.php
@@ -29,14 +29,15 @@ function register_block() {
 	$editor_style    = 'build/index.css';
 	$frontend_style  = 'build/frontend.css';
 	$frontend_script = 'build/frontend.js';
+	$plugin_dir      = plugin_dir_path( __FILE__ );
 
 	// Verify we have an editor script.
-	if ( ! file_exists( plugin_dir_path( __FILE__ ) . $editor_script ) ) {
+	if ( ! file_exists( $plugin_dir . $editor_script ) ) {
 		wp_die( esc_html__( 'Whoops! You need to run `npm run build` for Aurelius first.', 'aubreypwd-aurelius' ) );
 	}
 
 	// Autoload dependencies and version.
-	$asset_file = require plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+	$asset_file = require $plugin_dir . 'build/index.asset.php';
 
 	// Register editor script.
 	wp_register_script(
@@ -48,22 +49,22 @@ function register_block() {
 	);
 
 	// Register editor style.
-	if ( file_exists( plugin_dir_path( __FILE__ ) . $editor_style ) ) {
+	if ( file_exists( $plugin_dir . $editor_style ) ) {
 		wp_register_style(
 			'aubreypwd/aurelius/editor-style',
 			plugins_url( $editor_style, __FILE__ ),
 			[ 'wp-edit-blocks' ],
-			filemtime( plugin_dir_path( __FILE__ ) . $editor_style )
+			filemtime( $plugin_dir . $editor_style )
 		);
 	}
 
 	// Register frontend style.
-	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_style ) ) {
+	if ( file_exists( $plugin_dir . $frontend_style ) ) {
 		wp_register_style(
 			'aubreypwd/aurelius/style',
 			plugins_url( $frontend_style, __FILE__ ),
 			[],
-			filemtime( plugin_dir_path( __FILE__ ) . $frontend_style )
+			filemtime( $plugin_dir . $frontend_style )
 		);
 	}
 
@@ -75,7 +76,7 @@ function register_block() {
 	] );
 
 	// Register frontend script.
-	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_script ) ) {
+	if ( file_exists( $plugin_dir . $frontend_script ) ) {
 		wp_enqueue_script(
 			'aubreypwd/aurelius/frontend-script',
 			plugins_url( $frontend_script, __FILE__ ),
